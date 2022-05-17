a6f52d51e55f7425fa7e68bdd083f191b6d97d8e
moirasenff <103228001+moirasenff@users.noreply.github.com>
2022-05-16 21:23:58 -0400

    Updated Mapper.hpp

--- Mapper/Mapper/Mapper.hpp
+++ Mapper/Mapper/Mapper.hpp
@@ -8,28 +8,6 @@
 #define MAPPER_API __declspec(dllimport)
 #endif
 
-/* 
-#ifdef MAPPER_HPP
-#define MAPPER_HPP */
-
-#include <vector>
-#include <string>
-/*
-class Mapper
-{
-public:
-	extern "C" MAPPER_API explicit Mapper(int count = 0);
-	extern "C" MAPPER_API void map(int k, std::string c, std::string dir);
-	extern "C" MAPPER_API static bool test();
-
-private:
-
-	extern "C" MAPPER_API void exporter(std::vector<std::string> vect, std::string temp, int key);
-}
-; */
-
-//#endif MAPPER_H
-
 class MAPPER_API Mapper {
 public:
 	explicit Mapper(int count = 0);
